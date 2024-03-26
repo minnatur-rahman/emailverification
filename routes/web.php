f9dc6d1dcf8ab854701c419b2d72322e00592423
millatur <minnatur7373@gmail.com>
2024-03-26 10:14:15 +0600

    ushdsyug

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\StudentController;
+use App\Models\Student;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -23,3 +25,5 @@ Auth::routes([
 ]);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware('verified');
+
+Route::post('student/create', 'StudentController@create')->name('student.create');
